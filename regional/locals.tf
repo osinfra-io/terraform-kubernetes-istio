10f130d3d0f6df65b5698bd2f5fbe62bbb032ba3
Brett Curtis <brett@osinfra.io>
2024-08-25 12:47:51 -0400

    Disable global Datadog tracer

--- regional/locals.tf
+++ regional/locals.tf
@@ -10,9 +10,12 @@ locals {
     }
   EOF
 
-  istio_gateway_datadog_tracing = <<EOF
+  istio_gateway_proxy_config = <<EOF
     {
     \"tracing\":{\"datadog\":{\"address\":\"$(HOST_IP):8126\"}}
+    }
+    {
+    \"proxyMetadata\":{\"DD_ENV\":\"${var.environment}\",\"DD_SERVICE\":\"istio-gateway\",\"DD_VERSION\":\"${var.istio_version}\"}
     }
   EOF
 
