9685eb2d17cb42dfafcb07ef005dec9561743a86
Brett Curtis <brett@osinfra.io>
2024-10-29 19:25:30 -0400

    Remove unused Kubernetes provider and Google client configuration

--- tests/fixtures/default/regional/main.tf
+++ tests/fixtures/default/regional/main.tf
@@ -15,48 +15,6 @@ terraform {
   }
 }
 
-# Helm Provider
-# https://registry.terraform.io/providers/hashicorp/helm/latest
-
-provider "helm" {
-  kubernetes {
-
-    cluster_ca_certificate = base64decode(
-      data.google_container_cluster.this.master_auth[0].cluster_ca_certificate
-    )
-
-    host  = data.google_container_cluster.this.endpoint
-    token = data.google_client_config.current.access_token
-  }
-}
-
-# Kubernetes Provider
-# https://registry.terraform.io/providers/hashicorp/kubernetes/latest
-
-provider "kubernetes" {
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.this.master_auth[0].cluster_ca_certificate
-  )
-
-  host  = data.google_container_cluster.this.endpoint
-  token = data.google_client_config.current.access_token
-}
-
-# Google Client Config Data Source
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
-
-data "google_client_config" "current" {
-}
-
-# Google Container Cluster Data Source
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/container_cluster
-
-data "google_container_cluster" "this" {
-  name     = "mock-cluster"
-  location = "mock-region"
-  project  = "mock-project"
-}
-
 module "test" {
   source = "../../../../regional"
 
