45a5cb05301382c45d839b7c4284a7883a2c5360
Brett Curtis <brett@osinfra.io>
2024-11-03 13:57:07 -0500

    Enhance workspace parsing logic in helpers.tf to support structured names and improve environment, region, and zone extraction

--- shared/helpers.tf
+++ shared/helpers.tf
@@ -1,13 +1,21 @@
 # Terraform Core Child Module Helpers
 
 locals {
-  env = lookup(local.env_map, local.environment, "none")
+  # The regex is used to parse the workspace name into its components, the components are used to set the region, zone, and environment variables.
+  # This requires structured workspace names, the structured workspace names are defined by the regex.
 
-  environment = (
-    terraform.workspace == "default" ?
-    "mock-environment" :
-    regex(".*-(?P<environment>[^-]+)$", terraform.workspace)["environment"]
-  )
+  # Examples:
+  # main-sandbox -> region = null, zone = null, environment = sandbox
+  # us-east1-sandbox -> region = us-east1, zone = null, environment = sandbox
+  # us-east1-foo-sandbox -> region = us-east1, zone = null, environment = sandbox
+  # us-east1-a-sandbox -> region = us-east1, zone = a, environment = sandbox
+  # us-east1-a-foo-sandbox -> region = us-east1, zone = a, environment = sandbox
+
+  environment_regex = "-(non-production|sandbox|production)$"
+  region_regex      = "^(us-[a-z]+\\d+)"
+  zone_regex        = "^us-[a-z]+\\d+-([abcd])"
+
+  env = local.environment != null ? lookup(local.env_map, local.environment, "none") : null
 
   env_map = {
     "non-production" = "nonprod"
@@ -15,19 +23,25 @@ locals {
     "sandbox"        = "sb"
   }
 
-  region = (
+  environment = local.parsed_workspace.environment
+
+  parsed_workspace = (
     terraform.workspace == "default" ?
-    "mock-region" :
-    regex("^(?P<region>[^-]+-[^-]+)", terraform.workspace)["region"]
+    {
+
+      # We use mock providers when testing Terraform child modules, these values align with the test naming conventions.
+
+      environment = "mock-environment"
+      region      = "mock-region"
+      zone        = "mock-zone"
+    } :
+    {
+      environment = try(regex(local.environment_regex, terraform.workspace)[0], null)
+      region      = try(regex(local.region_regex, terraform.workspace)[0], null)
+      zone        = try(regex(local.zone_regex, terraform.workspace)[0], null)
+    }
   )
 
-  zone = (
-    terraform.workspace == "default" ?
-    "mock-zone" :
-    (
-      regex("^(?P<region>[^-]+-[^-]+)(?:-(?P<zone>[^-]+))?-.*$", terraform.workspace)["zone"] != "" ?
-      regex("^(?P<region>[^-]+-[^-]+)(?:-(?P<zone>[^-]+))?-.*$", terraform.workspace)["zone"] :
-      null
-    )
-  )
+  region = local.parsed_workspace.region
+  zone   = local.parsed_workspace.zone
 }
